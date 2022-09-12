1b082c151f510be9860be7fa0f35e391da7072bf
Samuel A. Mikell <sbug1442@gmail.com>
2022-09-12 19:29:24 -0400

    Added buttons and label for output.

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,71 @@
 use eframe::egui;
 
+use crate::calculator::Calculator;
+
 #[derive(Default)]
-pub struct App;
+pub(crate) struct App {
+    calculator: Calculator,
+}
 
 impl App {
-    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+    pub fn new(_cc: &eframe::CreationContext<'_>, calculator: Calculator) -> Self {
+        Self { calculator }
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            // TODO
+            if ui.button("Quit").clicked() {
+                // Exit Button
+                frame.close();
+            }
+            ui.label(&self.calculator.equation);
+            if ui.button("0").clicked() {
+                self.calculator.push_to_equation('0');
+            }
+            if ui.button("1").clicked() {
+                self.calculator.push_to_equation('1');
+            }
+            if ui.button("2").clicked() {
+                self.calculator.push_to_equation('2');
+            }
+            if ui.button("3").clicked() {
+                self.calculator.push_to_equation('3');
+            }
+            if ui.button("4").clicked() {
+                self.calculator.push_to_equation('4');
+            }
+            if ui.button("5").clicked() {
+                self.calculator.push_to_equation('5');
+            }
+            if ui.button("6").clicked() {
+                self.calculator.push_to_equation('6');
+            }
+            if ui.button("7").clicked() {
+                self.calculator.push_to_equation('7');
+            }
+            if ui.button("8").clicked() {
+                self.calculator.push_to_equation('8');
+            }
+            if ui.button("9").clicked() {
+                self.calculator.push_to_equation('9');
+            }
+            if ui.button("+").clicked() {
+                self.calculator.push_to_equation('+');
+            }
+            if ui.button("-").clicked() {
+                self.calculator.push_to_equation('-');
+            }
+            if ui.button("*").clicked() {
+                self.calculator.push_to_equation('*');
+            }
+            if ui.button("/").clicked() {
+                self.calculator.push_to_equation('/');
+            }
+            if ui.button("=").clicked() {
+                self.calculator.calculate();
+            }
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use app::App;
+use calculator::Calculator;
 
 extern crate eframe;
 
 mod app;
+mod calculator;
 
 fn main() {
+    let calculator = Calculator::new();
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Calculator",
         native_options,
-        Box::new(|cc| Box::new(App::new(cc))),
+        Box::new(|cc| Box::new(App::new(cc, calculator))),
     )
 }
