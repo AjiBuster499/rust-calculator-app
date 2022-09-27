b8ae0bcf60fdfe91cabc1eac337e6b9f5ce10cf3
Samuel A. Mikell <sbug1442@gmail.com>
2022-09-27 13:40:36 -0400

    Adjusted button layout.

--- src/app.rs
+++ src/app.rs
@@ -29,37 +29,40 @@ impl eframe::App for App {
             ui.label(&self.calculator.display_equation);
             // TODO: Clean this up
             ui.columns(4, |cols| {
-                if cols[0].button("0").clicked() {
-                    self.calculator.push_to_equation("0");
-                }
-                if cols[0].button("1").clicked() {
-                    self.calculator.push_to_equation("1");
+                if cols[0].button("7").clicked() {
+                    self.calculator.push_to_equation("7");
                 }
                 if cols[0].button("4").clicked() {
                     self.calculator.push_to_equation("5");
                 }
-                if cols[0].button("7").clicked() {
-                    self.calculator.push_to_equation("7");
+                if cols[0].button("1").clicked() {
+                    self.calculator.push_to_equation("1");
                 }
-                if cols[1].button("2").clicked() {
-                    self.calculator.push_to_equation("2");
-                }
-                if cols[1].button("5").clicked() {
-                    self.calculator.push_to_equation("5");
+                if cols[0].button("0").clicked() {
+                    self.calculator.push_to_equation("0");
                 }
                 if cols[1].button("8").clicked() {
                     self.calculator.push_to_equation("8");
                 }
-                if cols[2].button("3").clicked() {
-                    self.calculator.push_to_equation("3");
+                if cols[1].button("5").clicked() {
+                    self.calculator.push_to_equation("5");
                 }
-                if cols[2].button("6").clicked() {
-                    self.calculator.push_to_equation("6");
+                if cols[1].button("2").clicked() {
+                    self.calculator.push_to_equation("2");
+                }
+                if cols[1].button(".").clicked() {
+                    self.calculator.push_to_equation(".");
                 }
                 if cols[2].button("9").clicked() {
                     self.calculator.push_to_equation("9");
                 }
-                if cols[3].button("=").clicked() {
+                if cols[2].button("6").clicked() {
+                    self.calculator.push_to_equation("6");
+                }
+                if cols[2].button("3").clicked() {
+                    self.calculator.push_to_equation("3");
+                }
+                if cols[2].button("=").clicked() {
                     self.calculator.push_to_equation(" )");
                     self.calculator.calculate();
                 }
@@ -75,6 +78,8 @@ impl eframe::App for App {
                 if cols[3].button("/").clicked() {
                     self.calculator.push_to_equation(" / ");
                 }
+                // Is negation possible with our current equation implementation?
+                // if cols[3].button("±").clicked() {}
             });
         });
     }
